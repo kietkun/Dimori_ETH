955369e6364b85f20e02c6f6f7c47cb4af439432
Kiệt Nguyễn <troycarterxyz@gmail.com>
2023-03-05 01:37:21 +0700

    change to matic

--- src/utils/contracts-config.js
+++ src/utils/contracts-config.js
@@ -1,5 +1,5 @@
 
-      export const contractAddress = "0x487663EE58dcCdCBfDDB19A2E9E8b18448b08B12"
-      export const ownerAddress = "0x7A76097050BdA9329b3d2563613cB68C2717a244"
-      export const networkDeployedTo = "5"
+      export const contractAddress = "0xd5Dc9BeA6f71dD367eB16FFAB25ce42dd48f71fE"
+      export const ownerAddress = "0x86BB580bce2889e95B37866Ad878bA73fc28597A"
+      export const networkDeployedTo = "80001"
     
